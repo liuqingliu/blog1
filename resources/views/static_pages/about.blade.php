f669e67092d2466c6b9aaa086584485c74cfb68b
liuqing <609163616@qq.com>
2018-05-30 08:26:37 +0000

    finish static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title','zhuye')
+
+@section('content')
+	<h1>about<h1>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title','zhuye')
+
+@section('content')
+	<h1>zhuye<h1>
+@stop
